fbb2fd7ae379198c0c4b1bc2d53b7bc32b65f5ee
teoma145 <matteodark145@yahoo.it>
2024-03-20 21:23:02 +0100

    fix

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -16,26 +16,20 @@ class Activity extends Model
     public function allPreviousPadriWorked(): bool
     {
 
-        if ($this->padre === null) {
-            return true;
-        }
 
-
-        if ($this->padre === 1) {
-            return true;
-        }
-
-        $samePadri = self::where('padre', $this->padre)->get();
-
-
-        foreach ($samePadri as $padre) {
-            if (!$padre->lavorata) {
-                return false;
-            }
-        }
-
+    if ($this->padre === null) {
         return true;
     }
+
+
+    $previousPadri = self::where('id', '<', $this->padre)->get();
+    foreach ($previousPadri as $padre) {
+        if (!$padre->lavorata || !$padre->allPreviousPadriWorked()) {
+            return false;
+        }
+    }
+    return true;
+}
     public function isLeaf(): bool
     {
 
